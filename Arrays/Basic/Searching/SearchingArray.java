1a3e8be4eaf93c9555141adecbf100edfc0a1999
Pankaj Kulsange <65228766+Punisher2501@users.noreply.github.com>
2022-06-20 10:28:56 +0530

    Searching in Array(linear & binary search)

--- Arrays/Basic/Searching/SearchingArray.java
+++ Arrays/Basic/Searching/SearchingArray.java
@@ -2,11 +2,14 @@ package Arrays.Basic.Searching;
 
 public class SearchingArray {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7};
+        int arr[] = {1,2,3,4,5,6,7,8,9};
         int find = 4;
         // return index 
         int index = linearSearch(arr, find);
         System.out.println(arr[index] + " is in array");
+        // ***** Note ***** : array must be sorted.
+        int index2 = binarySearch(arr, find);
+        System.out.println(arr[index2] + " is in array");
 
     }
     public static int linearSearch(int arr[], int find){
@@ -33,4 +36,17 @@ public class SearchingArray {
         }
         return Integer.MIN_VALUE;
     }
+    public static int binarySearch(int arr[], int find){
+        // code for asc sorted array
+        int i = 0, j = arr.length - 1;
+        int k = (i + j)/2;
+        while(i < j){
+            if(find == arr[k]) return k;
+            else if(find < arr[k]) j = k;
+            else i = k;
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    
 }
